08e783c7cf10cd1c6e2b370133c5b26849ad82a4
JordanWalling <jordanwalling1991@gmail.com>
2023-12-18 06:46:31 +1100

    complete arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
-
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+
+const multiply = num1 * num2;
+console.log(multiply);
+
+// random
+
+const random = Math.floor(Math.random() * 10);
+console.log(random);
+
+// modulus operator
+// remainder equal to 4
+
+const num3 = 20;
+const num4 = 8;
+const mod = num3 % num4;
+console.log(mod);
+
+// return highest number in the set
+
+const max = Math.max(20, 6);
+console.log(max);
